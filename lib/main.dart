f23b553cb24a138d2d5a4589d168132b05d856e9
Mohamed Nguira <m4moupro@gmail.com>
2022-06-10 22:44:30 +0300

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,42 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 import 'package:innocart/signup/signup.dart';
-
+import 'package:innocart/login/login.dart';
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
+
+  @override
+  State<StatefulWidget> createState() => MainState();
+}
+class MainState extends State<MyApp>{
+  bool signup = true;
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-    home: const MyHomePage(title: 'Flutter Demo Home Page'),
-      
+    return  MaterialApp(
+      home: signup?SignUpPage(title: "Signup",onTouched: (){
+        setState((){
+          signup = false;
+        });
+
+      }):LogInPage(title: "Login",onTouched: (){
+        setState((){
+          signup = true;
+        });
+
+      }),
+
     );
   }
+
 }
 
 
--- lib/signup/signup.dart
+++ lib/signup/signup.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:innocart/main.dart';
 
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
+class SignUpPage extends StatefulWidget {
+  const SignUpPage({Key? key, required this.title, required this.onTouched}) : super(key: key);
+  final Function onTouched;
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
   // how it looks.
@@ -16,58 +16,156 @@ class MyHomePage extends StatefulWidget {
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<SignUpPage> createState() => _SignUpState();
 }
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _SignUpState extends State<SignUpPage> {
+  bool checked = false;
   String email = "",password = "";
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
+    double height = size.height, width = size.width;
     return Scaffold(
 
-
           body: SingleChildScrollView(
-
-            child: Column(
-
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-
-                SizedBox(height: 100,
-                  child: new Image.asset("assets/images/logo.png",height: 200, width: 400)
+              scrollDirection: Axis.vertical,
+              child: Column (children: <Widget>[
+                Container(
+                    width: width,
+                    height: height * 0.2,
+                    decoration: const BoxDecoration(
+                      color : Color.fromRGBO(137, 141, 247, 1),
+                    ),
+                    child: Image.asset("assets/images/logo.png")
                 ),
-                TextField(
-                  obscureText: false,
-                  onChanged: (String value) async{
+                const SizedBox(height: 20),
+                SizedBox(width: 0.8 * width,height: 0.65 * height,child:
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text('Sign up', textAlign: TextAlign.left, style: TextStyle(
+                        color: Color.fromRGBO(0, 0, 0, 1),
+                        fontFamily: 'Manjari',
+                        fontSize: 25,
+                        letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+                        fontWeight: FontWeight.normal,
+                        height: 1
+                    ),),
+                    Row(children:  [
+                      const Text('Already have an account?', textAlign: TextAlign.left, style: TextStyle(
+                          color: Color.fromRGBO(0, 0, 0, 1),
+                          fontFamily: 'Manjari',
+                          fontSize: 14,
+                          letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+                          fontWeight: FontWeight.normal,
+                          height: 1
+                      ),),
+                      TextButton(onPressed: () {
+                        super.widget.onTouched();
+                  }, child: Text("Log in"))
+                    ]),
+                    TextField(
+                      obscureText: false,
+                      onChanged: (String value) async{
 
-                    setState((){
-                      email = value;
-                    });
-                  },
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Email',
-                  ),
-                ),
-                SizedBox(height: 10),
-                TextField(
-                  obscureText: true,
-                  onChanged: (String value) async{
+                        setState((){
+                          password = value;
+                        });
+                      },
+                      decoration: const InputDecoration(
+                        border: const OutlineInputBorder(),
+                        labelText: 'Name',
+                      ),
+                    ),SizedBox(height:20),
+                    TextField(
+                      obscureText: false,
+                      onChanged: (String value) async{
 
-                      setState((){
-                        password = value;
-                      });
-                  },
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Password',
-                  ),
+                        setState((){
+                          password = value;
+                        });
+                      },
+                      decoration: const InputDecoration(
+                        border: const OutlineInputBorder(),
+                        labelText: 'Email',
+                      ),
+                    ),SizedBox(height:20),
+                    TextField(
+                      obscureText:false,
+                      onChanged: (String value) async{
+
+                        setState((){
+                          password = value;
+                        });
+                      },
+                      decoration: const InputDecoration(
+                        border: const OutlineInputBorder(),
+                        labelText: 'Phone Number',
+                      ),
+                    ),SizedBox(height:20),
+                    TextField(
+                      obscureText: true,
+                      onChanged: (String value) async{
+
+                        setState((){
+                          password = value;
+                        });
+                      },
+                      decoration: const InputDecoration(
+                        border: const OutlineInputBorder(),
+                        labelText: 'Password',
+                      ),
+                    ),SizedBox(height:20),
+                    Row(children: [Checkbox(
+                      checkColor: Colors.white,
+                      value: checked,
+                      onChanged: (bool? value) {
+                        setState(() {
+                          checked = value!;
+                        });
+                      },
+                    ),
+                      Text('I agree on the terms of use', textAlign: TextAlign.left, style: const TextStyle(
+                          color: Colors.black,
+                          fontFamily: 'Manjari',
+                          fontSize: 14,
+                          letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+                          fontWeight: FontWeight.normal,
+                          height: 1
+                      ),)
+                    ]
+                    )
+                    ,SizedBox(height:20),
+                    Row(mainAxisAlignment: MainAxisAlignment.center,children: [
+                      TextButton(
+                        style: ButtonStyle(
+                          fixedSize: MaterialStateProperty.all(Size(300,50)),
+                          backgroundColor: MaterialStateProperty.resolveWith<Color?>(
+                                (Set<MaterialState> states) {
+                              if (states.contains(MaterialState.hovered))
+                                return Colors.blue.withOpacity(0.7);
+                              if (states.contains(MaterialState.focused) ||
+                                  states.contains(MaterialState.pressed))
+                                return Colors.blue.withOpacity(0.6);
+                              return Colors.blue; // Defer to the widget's default.
+                            },
+                          ),
+                          foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                        ),
+                        onPressed: () { },
+                        child: Text('Sign up'),
+                      )
+                    ],)
+
+                  ],
+
+
                 )
+                  ,)
                 ,
-              ],
-            ),
-          ),
+
+    ]
+          ))
 
     );
   }
