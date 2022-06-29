0ecdee20ebc7765445288ede432d9ea1cffffb74
angel_in_a_jar <ruslan_tyu@mail.ru>
2022-06-29 15:57:07 +0300

    tests fix

--- backend/tests/test_views.py
+++ backend/tests/test_views.py
@@ -9,10 +9,14 @@ from users.models import User
 class DeliveryViewTest(TestCase):
     @classmethod
     def setUpTestData(cls):
-        number_of_delivery = 13
+        User.objects.create(token='123', passwordMd5hash='qwerty12345', name='Del',
+                            phoneNumber='88005553535',
+                            email='yepper@gmail.com', age='14', deliveryRate=5.0,
+                            createdOrdersHistoryIds=[123, 456], deliveredOrdersHistoryIds=[45])
+        number_of_delivery = 2
         for delivery_num in range(number_of_delivery):
-            Delivery.objects.create(customerId=delivery_num, angelId=delivery_num + 1, orderId=delivery_num + 2,
-                                    estimatedTime='20:00', customerConfirmation=True, angelConfirmation=True)
+            Delivery.objects.create(customerId=delivery_num, angelId=1, orderId=delivery_num + 2,
+                                    customerConfirmation=True, angelConfirmation=True)
 
     def test_view_url_exists_at_desired_location(self):
         resp = self.client.get('/delivery/?token=123')
@@ -24,10 +28,9 @@ class DeliveryViewTest(TestCase):
 
     def test_post_url_exists_at_desired_location(self):
         resp = self.client.post('/delivery/?token=123', {
-            "customerId": 12,
-            "angelId": 123,
+            "customerId": 1,
+            "angelId": 1,
             "orderId": 90,
-            "estimatedTime": "20:00",
             "customerConfirmation": True,
             "angelConfirmation": True
         })
@@ -35,21 +38,20 @@ class DeliveryViewTest(TestCase):
 
     def test_post_url_accessible_by_name(self):
         resp = self.client.post(reverse('delivery') + '?token=123', {
-            "customerId": 12,
-            "angelId": 123,
+            "customerId": 1,
+            "angelId": 1,
             "orderId": 90,
-            "estimatedTime": "20:00",
             "customerConfirmation": True,
             "angelConfirmation": True
         })
         self.assertEqual(resp.status_code, 201)
 
     def test_view_url_by_order_exists_at_desired_location(self):
-        resp = self.client.get('/delivery/1?token=123')
+        resp = self.client.get('/delivery/3?token=123')
         self.assertEqual(resp.status_code, 200)
 
     def test_view_url_by_order_accessible_by_name(self):
-        resp = self.client.get(reverse('delivery by id', args=[1]) + '?token=123')
+        resp = self.client.get(reverse('delivery by id', args=[3]) + '?token=123')
         self.assertEqual(resp.status_code, 200)
 
     def test_view_url_not_found_at_desired_location(self):
@@ -59,6 +61,20 @@ class DeliveryViewTest(TestCase):
     def test_view_url_not_found_by_name(self):
         resp = self.client.get(reverse('delivery by id', args=[99]) + '?token=123')
         self.assertEqual(resp.status_code, 404)
+
+    def test_put_url_exists_at_desired_location(self):
+        resp = self.client.put('/delivery/3?token=123', {
+            "customerConfirmation": "true",
+            "angelConfirmation": "true"
+        }, content_type='application/json')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_put_url_accessible_by_name(self):
+        resp = self.client.put(reverse('delivery') + '3?token=123', {
+            "customerConfirmation": "true",
+            "angelConfirmation": "true"
+        }, content_type='application/json')
+        self.assertEqual(resp.status_code, 200)
 
 
 class FeedbackViewTest(TestCase):
@@ -74,9 +90,13 @@ class OrdersViewTest(TestCase):
     @classmethod
     def setUpTestData(cls):
         number_of_orders = 5
+        User.objects.create(token='123', passwordMd5hash='qwerty12345', name='Del',
+                            phoneNumber='88005553535',
+                            email='yepper@gmail.com', age='14', deliveryRate=5.0,
+                            createdOrdersHistoryIds=[123, 456], deliveredOrdersHistoryIds=[45])
         for order_num in range(number_of_orders):
             Order.objects.create(customerId=order_num, name='Del', description='seven years bad luck',
-                                 expectedDeliveryTime='04:00', status='0', weight='100', cost='100',
+                                 expectedDeliveryTime='04:00:00', status='0', weight='100', cost='100',
                                  fee='50', address='Inno basement', possibleAngelsIds=[123, 456],
                                  picture='https://qwerty.com')
 
@@ -123,10 +143,10 @@ class OrdersViewTest(TestCase):
 
     def test_post_url_exists_at_desired_location(self):
         resp = self.client.post('/orders/?token=123', {
-            "customerId": 12,
+            "customerId": 1,
             "name": "Jack",
             "description": "1234",
-            "expectedDeliveryTime": "2022-06-20",
+            "expectedDeliveryTime": "04:00:00",
             "status": "2",
             "weight": "55.00",
             "cost": 55,
@@ -142,10 +162,10 @@ class OrdersViewTest(TestCase):
 
     def test_post_url_accessible_by_name(self):
         resp = self.client.post(reverse('orders') + '?token=123', {
-            "customerId": 12,
+            "customerId": 1,
             "name": "Jack",
             "description": "1234",
-            "expectedDeliveryTime": "2022-06-20",
+            "expectedDeliveryTime": "04:00:00",
             "status": "2",
             "weight": "55.00",
             "cost": 55,
@@ -202,8 +222,8 @@ class UserViewTest(TestCase):
         number_of_users = 10
         for user_num in range(number_of_users):
             User.objects.create(token='123', passwordMd5hash='qwerty12345', name='Del ' + str(user_num),
-                                phoneNumber='88005553535',
-                                email='yepper@gmail.com', age='14', deliveryRate=5.0,
+                                phoneNumber='88005553535 ' + str(user_num),
+                                email='yepper@gmail.com ' + str(user_num), age='14', deliveryRate=5.0,
                                 createdOrdersHistoryIds=[123, 456], deliveredOrdersHistoryIds=[45])
 
     def test_view_url_by_user_exists_at_desired_location(self):
@@ -222,16 +242,30 @@ class UserViewTest(TestCase):
         resp = self.client.get(reverse('user by id', args=[99]) + '?token=123')
         self.assertEqual(resp.status_code, 404)
 
-    def test_post_url_sign_in_exists_at_desired_location(self):
+    def test_post_url_sign_in_exists_at_desired_location_number(self):
         resp = self.client.post('/sign_in/', {
-            "name": "Del 0",
+            "name": "88005553535 0",
             "passwordMd5hash": "qwerty12345"
         })
         self.assertEqual(resp.status_code, 200)
 
-    def test_post_url_sign_in_accessible_by_name(self):
+    def test_post_url_sign_in_accessible_by_name_number(self):
         resp = self.client.post(reverse('sign_in'), {
-            "name": "Del 0",
+            "name": "88005553535 0",
+            "passwordMd5hash": "qwerty12345"
+        })
+        self.assertEqual(resp.status_code, 200)
+
+    def test_post_url_sign_in_exists_at_desired_location_email(self):
+        resp = self.client.post('/sign_in/', {
+            "name": "yepper@gmail.com 0",
+            "passwordMd5hash": "qwerty12345"
+        })
+        self.assertEqual(resp.status_code, 200)
+
+    def test_post_url_sign_in_accessible_by_name_email(self):
+        resp = self.client.post(reverse('sign_in'), {
+            "name": "yepper@gmail.com 0",
             "passwordMd5hash": "qwerty12345"
         })
         self.assertEqual(resp.status_code, 200)
